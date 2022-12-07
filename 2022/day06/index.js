a290449c7c05bf2774db36b582226f15057ac0d0
Michael Graybeal <lordgraysith@gmail.com>
2022-12-06 21:26:16 -0700

    day 6 done

--- 2022/day06/index.js
+++ 2022/day06/index.js
@@ -4,15 +4,25 @@ async function run() {
     const file = fs.readFileSync('./input.txt', {
         encoding: 'utf-8'
     })
-    for(let s = 0, e = 4; e < file.length; s++, e++) {
+    for(let s = 0, e = 14; e < file.length; s++, e++) {
       const sub = file.substring(s, e)
       const map = {
         [sub[0]]:1,
         [sub[1]]:1,
         [sub[2]]:1,
         [sub[3]]:1,
+        [sub[4]]:1,
+        [sub[5]]:1,
+        [sub[6]]:1,
+        [sub[7]]:1,
+        [sub[8]]:1,
+        [sub[9]]:1,
+        [sub[10]]:1,
+        [sub[11]]:1,
+        [sub[12]]:1,
+        [sub[13]]:1,
       }
-      if (Object.keys(map).length >= 4) {
+      if (Object.keys(map).length >= 14) {
         return e
       }
     }
