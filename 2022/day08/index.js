ee62e5aebd457122c7d53707314dc7f6df298046
Michael Graybeal <lordgraysith@gmail.com>
2022-12-08 17:46:40 -0700

    day 2 part 2

--- 2022/day08/index.js
+++ 2022/day08/index.js
@@ -6,17 +6,77 @@ async function run() {
   })
   const lines = file.split('\n')
   const grid = lines.map(line => line.split('').map(Number))
-  let visibleTrees = 0
+  let topTreeScore = 0
 
   for (let i = 0; i < grid.length; i++) {
     const row = grid[i]
     for (let j = 0; j < row.length; j++) {
-      if (isVisible(grid, i, j)) {
-        visibleTrees++
-      }
+      topTreeScore = Math.max(topTreeScore, getTreeViewScore(grid, i, j))
     }
   }
-  return visibleTrees
+  return topTreeScore
+}
+
+function getTreeViewScore(grid, row, col) {
+  if(row === 0
+    || col === 0
+    || row === grid.length - 1
+    || col === grid[0].length - 1) {
+    return 0
+  }
+
+  return countTreesLeft(grid, row, col)
+    * countTreesRight(grid, row, col)
+    * countTreesUp(grid, row, col)
+    * countTreesDown(grid, row, col)
+}
+
+function countTreesLeft(grid, row, col) {
+  const height = grid[row][col]
+  let treeCount = 0
+  for (let i = col - 1; i >= 0; i--) {
+    treeCount++
+    if (grid[row][i] >= height) {
+      break
+    }
+  }
+  return treeCount
+}
+
+function countTreesRight(grid, row, col) {
+  const height = grid[row][col]
+  let treeCount = 0
+  for (let i = col + 1; i < grid[0].length; i++) {
+    treeCount++
+    if (grid[row][i] >= height) {
+      break
+    }
+  }
+  return treeCount
+}
+
+function countTreesUp(grid, row, col) {
+  const height = grid[row][col]
+  let treeCount = 0
+  for (let i = row - 1; i >= 0; i--) {
+    treeCount++
+    if (grid[i][col] >= height) {
+      break
+    }
+  }
+  return treeCount
+}
+
+function countTreesDown(grid, row, col) {
+  const height = grid[row][col]
+  let treeCount = 0
+  for (let i = row + 1; i < grid.length; i++) {
+    treeCount++
+    if (grid[i][col] >= height) {
+      break
+    }
+  }
+  return treeCount
 }
 
 function isVisible(grid, row, col) {
